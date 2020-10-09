e3760f2ef280cbb8884a7e176d25ad35765cd662
Orveberg <randomorven@gmail.com>
2020-10-09 15:09:36 +0200

    började på 4630

---
+++ 4630/Anstalld.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace _4630
+{
+    abstract class Anstalld
+    {
+        protected String fornamn;
+        protected String efternamn;
+
+        abstract public double Beraknalon();
+        public override string ToString()
+        {
+                return fornamn + efternamn;
+
+          }
+       
+
+
+
+
+
+    
+
+        
+        
+
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ 4630/Test.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace _4630
+{
+
+     class Test
+    {
+
+        
+    }
+
+
+
+
+
+}
