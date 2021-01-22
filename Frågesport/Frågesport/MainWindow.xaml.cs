2499f26bf6a193b6eb0d77a87b331725bdc5becd
Orveberg <71634033+Orveberg@users.noreply.github.com>
2021-01-22 14:29:18 +0100

    gjorde klar del 1

--- Frågesport/Frågesport/MainWindow.xaml.cs
+++ Frågesport/Frågesport/MainWindow.xaml.cs
@@ -20,11 +20,27 @@ namespace Frågesport
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int CurrentQ = 0;
+        List<Questioncard> frågor = new List<Questioncard>();
+        Questioncard q;
         public MainWindow()
         {
             InitializeComponent();
-        }
 
+            frågor.Add(new Questioncard("Vad hete Danmarks Huvudstad?", "Köpenhamn"));
+
+            frågor.Add(new Questioncard("Vad heter Norges Huvudstad?", "Oslo"));
+
+
+            frågor.Add(new Questioncard("Vad hete Finlands Huvudstad?", "Helsingfors"));
+
+            q = frågor[CurrentQ];
+            Fråga1.Text = q.question;
+
+
+
+        }
+         
         private void Svar1_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -32,7 +48,26 @@ namespace Frågesport
 
         private void Nästafråga_Click(object sender, RoutedEventArgs e)
         {
+            CurrentQ = CurrentQ + 1;
+            Fråga1.Text = frågor[CurrentQ].question;
+            Svar1.Text = "";
+            feedback.Text = "";
 
+        }
+
+        private void Rätta_Click(object sender, RoutedEventArgs e)
+        {
+
+            String uans = Svar1.Text;
+            if (uans == frågor[CurrentQ].answer)
+            {
+                feedback.Text = "Rätt";
+
+            }
+            else
+            {
+                feedback.Text = "Fel";
+            }
         }
     }
 }
---
+++ Frågesport/Frågesport/Questioncard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frågesport
+{
+    class Questioncard
+    {
+        public String question { get; }
+
+        public String answer { get; }
+
+        public Questioncard(string q, string ans)
+            {
+                question = q;
+                answer = ans;
+            }
+
+
+
+    }
+}
