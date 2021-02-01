c2689416f8a054d1b1f11af96d81f8c832ee3e80
Orveberg <71634033+Orveberg@users.noreply.github.com>
2021-02-01 11:39:27 +0100

    Klar med fil

--- Frågesport/Frågesport/MainWindow.xaml.cs
+++ Frågesport/Frågesport/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Path = System.IO.Path;
+
 
 namespace Frågesport
 {
@@ -23,9 +26,19 @@ namespace Frågesport
         private int CurrentQ = 0;
         List<Questioncard> frågor = new List<Questioncard>();
         Questioncard q;
+        private string filename = "frågor.txt";
+        private string pathAndFilename;
         public MainWindow()
         {
-            InitializeComponent();
+
+           
+        InitializeComponent();
+            string docPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            pathAndFilename = Path.Combine(docPath, filename);
+
+            ReadFile();
+
 
             frågor.Add(new Questioncard("Vad hete Danmarks Huvudstad?", "Köpenhamn"));
 
@@ -69,5 +82,30 @@ namespace Frågesport
                 feedback.Text = "Fel";
             }
         }
-    }
+        private void ReadFile()
+        {
+            try
+            {
+                // Open the text file using a stream reader.
+                using (var sr = new StreamReader(pathAndFilename))
+                {
+                    string q;
+                    string ans;
+                   ans = sr.ReadLine();
+                    q = sr.ReadLine();
+                    while(q != null)
+                    {
+                        frågor.Add(new Questioncard(ans, q));
+                        ans = sr.ReadLine();
+                        q = sr.ReadLine();
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Fråga1.Text = "The file could not be read:" + e.Message;
+            }
+        }
+        }
 }
